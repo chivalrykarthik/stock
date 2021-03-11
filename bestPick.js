ef6b736278fd7cbe3522dc17efe57ca26116c762
Karthik Arumugam <chivalrykarthik@gmail.com>
2021-03-11 23:29:38 +0530

    Added filters

--- bestPick.js
+++ bestPick.js
@@ -1,215 +1,156 @@
 const csv = require('csvtojson')
-
 const { Parser } = require('json2csv');
-
-const fs = require('fs');
-
+const { writeFile } = require('fs');
+const { promisify } = require('util');
+const writePr = promisify(writeFile);
 const csvFilePath = './soft.csv';
-
 const output = './bestpick.csv';
-
 const log = console.log;
 
- 
-
 const metrics = {
-
-    "price":{
-
-        validate:false,
-
+    "price": {
+        validate: true,
         value: 200,
-
-        operator: function(v) { return v > this.value },
-
-        col:'CMP\n          Rs.' // Current price
-
+        operator: function (v) { return v > this.value },
+        col: 'CMP' // Current price
     },
-
-    pe:{
-
-        validate:false,
-
+    pe: {
+        validate: false,
         value: 16,
-
-        operator: function(v) { return v > this.value },
-
-        col:'P\/E' // Pe ratio
-
+        operator: function (v) { return v > this.value },
+        col: 'P\/E' // Pe ratio
     },
-
-    peg:{
-
-        validate:true,
-
+    peg: {
+        validate: true,
         value: 1,
-
-        operator: function(v) { return v > this.value },
-
-        col:'PEG' // PG ratio
-
+        operator: function (v) { return v > this.value || v < 0 },
+        col: 'PEG' // PG ratio
     },
-
-    interestCoverage:{
-
-        validate:true,
-
+    interestCoverage: {
+        validate: true,
         value: 2,
-
-        operator: function(v) { return v < this.value },
-
-        col:'Int Coverage' // Interest coverage ratio
-
+        operator: function (v, debt) {
+            if (v < 0) return true;
+            if (!isNaN(debt) && debt > 0 && v < this.value) return true;
+            return false;
+        },
+        col: 'Int Coverage' // Interest coverage ratio
     },
-
-    debt:{
-
-        validate:true,
-
+    debt: {
+        validate: true,
         value: 1.5,
-
-        operator: function(v) { return v > this.value },
-
-        col:'Debt / Eq' // Debt to equity ratio
-
+        operator: function (v) { return v > this.value },
+        col: 'Debt / Eq' // Debt to equity ratio
     },
-
-    quickRatio:{
-
-        validate:false,
-
+    quickRatio: {
+        validate: false,
         value: 1,
-
-        operator: function(v) { return v < this.value },
-
-        col:'Quick Rat' // Quick ratio
-
+        operator: function (v, debt) {
+            if (v < 0) return true;
+            if (!isNaN(debt) && debt > 0 && v < this.value) return true;
+            return false;
+        },
+        col: 'Quick Rat' // Quick ratio
     },
-
-    currentRatio:{
-
-        validate:true,
-
+    currentRatio: {
+        validate: true,
         value: 1.33,
-
-        operator: function(v) { return v < this.value },
-
-        col:'Current ratio' // Current ratio
-
+        operator: function (v, debt) {
+            if (v < 0) return true;
+            if (!isNaN(debt) && debt > 0 && v < this.value) return true;
+            return false;
+        },
+        col: 'Current ratio' // Current ratio
     }
-
 };
-
- 
-
 class BestPick {
-
-    readCsv = async()=>{
-
+    readCsv = async () => {
         try {
-
             const jsonArray = await csv().fromFile(csvFilePath);
-
             return jsonArray;
-
         } catch (e) {
-
             console.log("Error in reading csv")
-
         }
-
     }
 
-    filterStock = (stocks)=>{
-
-        const addFilters = (stock)=>{
-
+    filterStock = (stocks) => {
+        const addFilters = (stock) => {
             const price = metrics.price;
-
             const pe = metrics.pe;
-
             const peg = metrics.peg;
-
             const interCoverage = metrics.interestCoverage;
-
             const debt = metrics.debt;
-
             const quickRatio = metrics.quickRatio;
-
             const currentRatio = metrics.currentRatio;
-
-            //Price
-
-            if(price.validate && price.operator(stock[price.col])){
-
+            //Price            
+            if (price.validate && price.operator(stock[price.col])) {
                 return false;
-
+            }
+            //PE
+            if (pe.validate && pe.operator(stock[pe.col])) {
+                return false;
+            }
+            //PEG
+            if (peg.validate && peg.operator(stock[peg.col])) {
+                return false;
             }
 
-        }
+            //Interest coverage            
+            if (interCoverage.validate && interCoverage.operator(stock[interCoverage.col], stock[debt.col])) {
+                return false;
+            }
 
+            //Debt
+            if (debt.validate && debt.operator(stock[debt.col])) {
+                return false;
+            }
+
+            //Quick Ratio
+            if (quickRatio.validate && quickRatio.operator(stock[quickRatio.col], stock[debt.col])) {
+                return false;
+            }
+
+            //Current Ratio
+            if (currentRatio.validate && currentRatio.operator(stock[currentRatio.col], stock[debt.col])) {
+                return false;
+            }
+            return true;
+        }
         return stocks.filter(addFilters);
-
-        
-
     }
 
-    buildCsv = (data) => {        
-
+    buildCsv = (data) => {
         try {
-
             const fields = Object.keys(data[0]);
-
             const opts = { fields };
-
             const parser = new Parser(opts);
-
             const csv = parser.parse(data);
-
             return csv;
-
         } catch (err) {
-
             console.error(err);
-
         }
-
     }
 
- 
-
-    createCsv= async (content) => {        
-
-        try{
-
-            const wr = await writePr(output,content);
-
+    createCsv = async (content) => {
+        try {
+            const wr = await writePr(output, content);
             console.log("Completed");
-
-        } catch(e){
-
-            console.log("Error in writing:",e.message)
-
+        } catch (e) {
+            console.log("Error in writing:", e.message)
         }
-
     }
-
 }
 
- 
-
-const processStock = async ()=>{
-
+const processStock = async () => {
     const best = new BestPick();
-
     const jsonData = await best.readCsv();
-
     const bestStocks = best.filterStock(jsonData);
-
+    if (!bestStocks || !bestStocks.length) {
+        console.log(`No match found`);
+        return;
+    }
     const csvData = best.buildCsv(bestStocks);
-
     best.createCsv(csvData);
 
 }
-
 processStock();
--- read.js
+++ read.js
@@ -1,177 +1,75 @@
 const cheerio = require('cheerio');
-
 const { Parser } = require('json2csv');
-
 const { readFile, writeFile } = require('fs');
-
 const { promisify } = require('util');
-
 const readFilePr = promisify(readFile);
-
 const writePr = promisify(writeFile);
-
 let contentLength = null;
-
 class StockProcess {
-
     getHtml = async () => {
-
         try {
-
             const content = await readFilePr('./test.html', 'utf8');
-
             return content;
-
         } catch (e) {
-
             console.log("Error in reading file", e.messaged)
-
         }
-
     }
-
-
-
     processHtml = async (html) => {
-
         try {
-
             const $ = cheerio.load(html);
-
             const heading = [];
-
             let content = [];
-
             const getHeading = (key, val) => {
-
                 const txt = $(val).text().trim();
-
                 if (!heading.includes(txt)) heading.push(txt);
-
             }
-
-
-
             const getContent = (key, val) => {
-
                 const headingLenght = heading.length;
-
                 const index = key % headingLenght;
-
                 const headingTxt = heading[index];
-
                 const valTxt = $(val).text().trim();
-
                 if (index === 0) {
-
                     contentLength = contentLength !== null ? contentLength + 1 : 0;
-
                 }
-
-
-
                 content[contentLength] = content[contentLength] ? { ...content[contentLength], [headingTxt]: valTxt } : { [headingTxt]: valTxt };
-
-
-
             };
-
-
-
             $('table tbody tr th').each(getHeading);
-
             $('table tbody tr td').each(getContent);
-
             return content;
-
         } catch (e) {
-
             console.log("Invalid error:", e.message);
-
         }
-
     }
-
     buildCsv = (data) => {
-
         try {
-
             const fields = Object.keys(data[0]);
-
             const opts = { fields };
-
             const parser = new Parser(opts);
-
             const csv = parser.parse(data);
-
             return csv;
-
         } catch (err) {
-
             console.error(err);
-
         }
-
     }
-
-
-
     createCsv = async (content) => {
-
-        /*return new Promise((res, rej) => {
-
-            writeFile('./analysis.csv', content, (err) => {
-
-                if (err) return rej(err);
-
-                return res("Completed");
-
-            })
-
-        })*/
-
         try {
-
             const wr = await writePr('./analysis.csv', content);
-
             console.log("Completed");
-
         } catch (e) {
-
             console.log("Error in writing:", e.message)
-
         }
-
     }
-
 }
-
-
-
 const processFunc = async () => {
-
     try {
-
         const obj = new StockProcess();
-
         const html = await obj.getHtml();
-
         const content = await obj.processHtml(html);
-
         const csv = obj.buildCsv(content);
-
         obj.createCsv(csv);
-
     } catch (e) {
-
         console.log("Err:", e.message)
-
     }
-
-
-
 };
-
-
 
 processFunc();
