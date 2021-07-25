83ae6ecca6e76390666fde382fc5733940f7b6e4
Karthik Arumugam <chivalrykarthik@gmail.com>
2021-07-25 17:37:11 +0530

    Added new columns

--- scrapper.js
+++ scrapper.js
@@ -14,9 +14,10 @@ const output = process.env.OUTPUT;
 
 const removeSelectors = ['Price to Earning', 'Dividend yield', 'Net Profit latest quarter', 'YOY Quarterly sales growth', 'YOY Quarterly profit growth', 'Sales latest quarter', 'Return on capital employed'];
 const cols = [
-    ['Price to Earning', 'PEG Ratio', 'Price to Sales', 'EVEBITDA', 'Interest Coverage Ratio', 'Debt to equity', 'Current ratio', 'Return on equity', 'Average return on equity 3Years', 'Average return on equity 5Years', 'Return on capital employed', 'Average return on capital employed 3Years', 'Average return on capital employed 5Years', 'Sales growth'],
-    ['Sales growth 3Years', 'Sales growth 5Years', 'EPS', 'EPS last year', 'EPS preceding year', 'EPS latest quarter', 'EPS preceding year quarter', 'Dividend yield', 'Dividend Payout Ratio', 'Return on assets', 'Return on assets preceding year', 'Return on assets 3years', 'Return on assets 5years'],
-    ['Altman Z Score', 'G Factor']
+    ['Price to Earning', 'PEG Ratio', 'Price to Sales', 'EVEBITDA', 'Interest Coverage Ratio', 'Debt to equity', 'Current ratio', 'Return on equity', 'Return on equity preceding year', 'Average return on equity 3Years', 'Average return on equity 5Years', 'Return on capital employed', 'Return on capital employed preceding year', 'Average return on capital employed 3Years'],
+
+    ['Average return on capital employed 5Years', 'Sales growth', 'YOY Quarterly sales growth', 'Sales growth 3Years', 'Sales growth 5Years', 'EPS', 'EPS last year', 'EPS preceding year', 'EPS latest quarter', 'EPS preceding year quarter', 'Dividend yield', 'Dividend Payout Ratio'],
+    ['Return on assets', 'Return on assets preceding year', 'Return on assets 3years', 'Return on assets 5years', 'Altman Z Score', 'G Factor']
 ]
 
 const selectors = async (page, selector, isCheked, indx = 0) => {
