087748283c3461923b8d543b759cbfcae3d9a9f9
Karthik Arumugam <chivalrykarthik@gmail.com>
2021-10-23 16:58:48 +0530

    Updated selectors

--- selectors.js
+++ selectors.js
@@ -1,7 +1,7 @@
 const removeSelectors = ['Price to Earning', 'Dividend yield', 'Net Profit latest quarter', 'YOY Quarterly sales growth', 'YOY Quarterly profit growth', 'Sales latest quarter', 'Return on capital employed'];
 const cols = [
     ['Price to Earning', 'Earnings yield', 'PEG Ratio', 'Price to Sales', 'Price to Free Cash Flow', 'Price to Cash Flow', 'EVEBITDA', 'Interest Coverage Ratio', 'Debt to equity', 'Current ratio', 'Free cash flow last year', 'Free cash flow preceding year', 'Free cash flow 3years'],
-
+    ['QoQ Profits', 'QoQ Sales', 'YOY Quarterly profit growth'],
     ['Free cash flow 5years', 'Return on equity', 'Return on equity preceding year', 'Average return on equity 3Years', 'Average return on equity 5Years', 'Return on capital employed', 'Return on capital employed preceding year', 'Average return on capital employed 3Years'],
 
 
